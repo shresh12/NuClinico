d18082dbd2348f870e140de8392dd8fa54efe9e6
Shreshta S P <shreshta.pudakalkatti@qasolve.ai>
2021-02-04 19:04:24 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NuClinico
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+
+        }
+        public async void Navigate_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Dashboard());
+
+        }
+    }
+}
